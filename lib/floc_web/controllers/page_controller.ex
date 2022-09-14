db891f7bdc394d6fba23325342781d4ec0d890d4
Rick Runyon <rick.runyon@gmail.com>
2022-09-14 09:43:24 -0400

    Quote

--- lib/floc_web/controllers/page_controller.ex
+++ lib/floc_web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule FlocWeb.PageController do
   use FlocWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", quote: fetch_quote())
+  end
+
+  def fetch_quote() do
+    {:ok, response} = HTTPoison.get "https://zenquotes.io/api/quotes"
+    {:ok, parsed} = response.body |> Jason.decode
+    [head | tail] = parsed
+
+    %{text: head["q"], author: head["a"]}
   end
 end
